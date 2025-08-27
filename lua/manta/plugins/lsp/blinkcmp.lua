5182e35f30859980facc022f32603077add05b9d
Manta Express <mantaxrpresso@gmail.com>
2025-08-26 20:05:19 -0600

    Make blink.cmp work
    Made blinkcmp actually work (I needed to install fucking fzf-lua for
    some dumbass reason) customize it laterrrrr

--- lua/manta/plugins/lsp/blinkcmp.lua
+++ lua/manta/plugins/lsp/blinkcmp.lua
@@ -25,7 +25,13 @@ return {
 		-- C-k: Toggle signature help (if signature.enabled = true)
 		--
 		-- See :h blink-cmp-config-keymap for defining your own keymap
-		keymap = { preset = "enter" },
+		keymap = {
+			preset = "enter",
+			["<Tab>"] = { "select_next", "fallback" },
+			["<C-Tab>"] = { "select_next", "fallback" },
+			["<C-j>"] = { "select_next", "fallback" },
+			["<C-k>"] = { "select_prev", "fallback" },
+		},
 
 		appearance = {
 			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
@@ -39,7 +45,7 @@ return {
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+			default = { "snippets", "path", "lsp", "buffer" },
 		},
 
 		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
