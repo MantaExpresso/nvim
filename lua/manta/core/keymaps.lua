13e2e76cdf66037819f9f156434c530a15d5ce21
Manta Express <mantaxrpresso@gmail.com>
2025-08-26 21:28:07 -0600

    Fix FzfLua Keymaps and colors

--- lua/manta/core/keymaps.lua
+++ lua/manta/core/keymaps.lua
@@ -2,12 +2,10 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("i", "<C-i>", "<ESC>", { desc = "Normal mode without killing my pinky" })
-keymap.set("n", "<C-BS>", "d^")
+keymap.set("i", "<C-i>", "<ESC>")
 -- keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 keymap.set("n", "<leader>cc", "<cmd>Cord toggle<CR>")
--- keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>")
 
 keymap.set("n", "<leader>hh", "<cmd>BufferLineCyclePrev<CR>")
 keymap.set("n", "<leader>ll", "<cmd>BufferLineCycleNext<CR>")
@@ -16,5 +14,9 @@ keymap.set("n", "<leader>dd", "<cmd>bd!<CR>")
 keymap.set("n", "<leader>st", "<cmd>ExportTable<CR>")
 keymap.set("n", "<leader>et", "<cmd>EasyTablesImportThisTable<CR>")
 
--- keymap.set("n", "<leader>ng", "<cmd>Neogit<CR>")
 keymap.set("n", "<leader>dv", "<cmd>DiffviewOpen<CR>")
+
+keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>")
+keymap.set("n", "<leader>fr", "<cmd>FzfLua oldfiles<cr>")
+keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>")
+keymap.set("n", "<leader>cm", "<cmd>delmarks A-Z0-9<cr>")
