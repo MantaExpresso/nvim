3ccc7e3fa55a94ae31548d95892a352ff62bd493
Manta Express <mantaxrpresso@gmail.com>
2025-10-13 17:37:43 -0600

    Add Go file motions and esc keymap for term

--- lua/manta/core/keymaps.lua
+++ lua/manta/core/keymaps.lua
@@ -34,3 +34,24 @@ keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>")
 keymap.set("n", "<leader>cm", "<cmd>delmarks A-Z0-9<cr>")
 
 keymap.set("n", "<leader>do", "<cmd>lua require('dapui').toggle()<cr>")
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "go" },
+	callback = function()
+		vim.schedule(function()
+			keymap.set("n", "<leader>gr", "<cmd>GoRun<CR>", { buffer = true })
+			-- You can set more keymaps here by following the above format
+		end)
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
+	group = nativeTermGroup,
+	pattern = "term://*",
+	callback = function(event)
+		local buf = event.buf
+		-- vim.keymap.set("t", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go to Left Window", buffer = buf, nowait = true })
+		-- vim.keymap.set("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to Above Window", buffer = buf, nowait = true })
+		vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode", buffer = buf, nowait = true })
+	end,
+})
